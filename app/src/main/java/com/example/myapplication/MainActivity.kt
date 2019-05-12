7198c12d7484fbc01c9dec9b393fe916201c7a0f
Daisuke Kato <kato.daisuke429@gmail.com>
2019-05-12 23:55:15 +0900

    update dependencies

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.myapplication
 
+import android.app.usage.UsageEvents
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.Button)
+        button.setOnClickListener {
+            Toast.makeText(this, "You clicked!",
+                Toast.LENGTH_SHORT).show()
+        }
     }
 }
